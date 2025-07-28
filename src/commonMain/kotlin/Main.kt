d483f2cdd9b712e15eb66f15d96681842b0fc54e
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-07-28 17:12:06 +0400

    Use `toDoubleOrNull` instead of `toDouble` to prevent crashes on invalid input

--- src/commonMain/kotlin/Main.kt
+++ src/commonMain/kotlin/Main.kt
@@ -2,7 +2,7 @@ package org.example
 
 fun main() {
     val testStr = ".1234587546821270405694314400000005"
-    println(testStr.toDouble())
+    println(testStr.toDoubleOrNull())
     println(testStr.parseFractionalPartOfDoubleInPlaceAsLong())
     println(testStr.parseFractionalPartOfDoubleInPlaceAsDouble())
 }
